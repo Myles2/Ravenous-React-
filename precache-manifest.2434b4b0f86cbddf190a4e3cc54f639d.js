7919fb5e8aaa295323e6aabbffb8c286c6be718e
Myles2 <winnerkd288@gmail.com>
2018-12-17 19:12:46 -0500

    Updates

---
+++ precache-manifest.2434b4b0f86cbddf190a4e3cc54f639d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d0905e66c87b168792e1cf920c613ee",
+    "url": "/Ravenous-React-/static/media/background_search_mobile.5d0905e6.jpg"
+  },
+  {
+    "revision": "902df4c501596c42571749648e081c71",
+    "url": "/Ravenous-React-/static/media/background_search_desktop.902df4c5.jpg"
+  },
+  {
+    "revision": "c2f9c921a574d44ed5fb",
+    "url": "/Ravenous-React-/static/js/runtime~main.c2f9c921.js"
+  },
+  {
+    "revision": "012382221835dbc59971",
+    "url": "/Ravenous-React-/static/js/main.01238222.chunk.js"
+  },
+  {
+    "revision": "d94fc685c375ad0a6b02",
+    "url": "/Ravenous-React-/static/js/1.d94fc685.chunk.js"
+  },
+  {
+    "revision": "012382221835dbc59971",
+    "url": "/Ravenous-React-/static/css/main.f71c7f30.chunk.css"
+  },
+  {
+    "revision": "878b0fea7dfa3738688868b866efa071",
+    "url": "/Ravenous-React-/index.html"
+  }
+];
